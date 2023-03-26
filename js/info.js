fe60cbec1dcdd1ce3b5b236fb52be340e9d427ce
Jiang Xiaowei <jiangxiaowei981109@gmail.com>
2023-03-26 00:54:46 -0400

    Update info.js

--- js/info.js
+++ js/info.js
@@ -1,223 +1,95 @@
 export var info = {
-    "first_name": "Zhaomin",
-    "last_name": "Wu",
-    "email": "zhaomin@u.nus.edu",
-    "address": "Prince George's Park Residences, 1 Prince George's Park, Singapore 118425",
+    "first_name": "Xiaowei",
+    "last_name": "Jiang",
+    "email": "xiaoweij@seas.upenn.edu",
+    "address": "Philadelphia, PA 19104",
     "phone": null,
-    "linkedin": "https://www.linkedin.com/in/zhaomin-wu-958159258/",
-    "github": "https://github.com/JerryLife",
-    "google_scholar": "https://scholar.google.com/citations?user=QjehmgkAAAAJ&hl",
-    "about_me": "I am a Ph.D candidate at the department of computer science in National University of Singapore (NUS), supervised by Prof. Bingsheng He. I obtained my Bachelor degree at Huazhong University of Science at Technology (HUST), supervised by Prof. Kun He. My research interest lies in privacy-preserving machine learning, including federated learning, differential privacy, and machine unlearning.",
+    "linkedin": "https://www.linkedin.com/in/xiaoweijiang/",
+    "github": "https://github.com/xynico",
+    "google_scholar": "https://scholar.google.com/citations?user=Sr9TeLEAAAAJ&hl=en&authuser=1&oi=ao",
+    "about_me": "I am a Master student at the department of Bioengineering in University of Pennsylvania (UPenn), supervised by Prof. Platt Michael and Prof. Konrad Kording. I obtained my Bachelor degree at Henan University (HENU), majored in Applied Psychology, supervised by Prof. Yanan Chen. My research interest lies in Cognitive Neuroscience and Brain-Computer Interface (BCI).",
     "education": [
         {
-            "school": "National University of Singapore (NUS)",
-            "region": "Singapore",
-            "degree": "Ph.D. Candidate",
-            "major": "Computer Science",
-            "start_date": "July 2019",
+            "school": "University of Pennsylvania (UPenn)",
+            "region": "Philadelphia, PA",
+            "degree": "DEGREES:
+            Master of Sci in Engineering",
+            "major": "Bioengineering",
+            "start_date": "September 2021",
             "end_date": "Present",
-            "description": "GPA: 4.67/5.0"
+            "description": "GPA: 3.96/4.0"
         },
         {
-            "school": "Huazhong University of Science and Technology (HUST)",
-            "region": "Wuhan, China",
-            "degree": "Bachelor of Engineering",
-            "major": "Computer Science and Technology",
-            "start_date": "September 2015",
-            "end_date": "June 2019",
-            "description": "GPA: 3.91/4.0; Rank: 10/281"
+            "school": "Henan University (HENU)",
+            "region": "Kaifeng, China",
+            "degree": "Bachelor of Science",
+            "major": "Applied Psychology",
+            "start_date": "September 2017",
+            "end_date": "June 2021",
+            "description": "Awarded 2021 Excellent Bachelor's Thesis in Henan Province"
         },
-        {
-            "school": "No.1 Middle School affiliated to Central China Normal University",
-            "region": "Wuhan, China",
-            "degree": "High School Diploma",
-            "major": "Science",
-            "start_date": "September 2012",
-            "end_date": "June 2015",
-            "description": "Score: 644/750"
-        }
     ],
     "publication": [
         {
-            "title": "DeltaBoost: Gradient Boosting Decision Trees with Efficient Machine Unlearning",
-            "authors": "Zhaomin Wu, Junhui Zhu, Qinbin Li, Bingsheng He",
-            "publication": "ACM SIGMOD International Conference on Management of Data",
-            "year": 2023,
-            "pub_abbr": "SIGMOD 2023",
-
-            "pub_link": null,
-            "code_link": null,
-            "pdf_link": null
-        },
-        {
-            "title": "A Coupled Design of Exploiting Record Similarity for Practical Vertical Federated Learning",
-            "authors": "Zhaomin Wu, Qinbin Li, Bingsheng He",
-            "publication": "36th Conference on Neural Information Processing Systems",
+            "title": "A Depression-Risk Mental Pattern Identified by Hidden Markov Model in Undergraduates",
+            "authors": "Jiang Xiaowei, Chen Yanan, Ao Na, Xiao Yyang, & Du Feng",
+            "publication": "International Journal of Environmental Research and Public Health",
             "year": 2022,
-            "pub_abbr": "NeurIPS 2022",
-            "pub_link": "https://proceedings.neurips.cc/paper_files/paper/2022/hash/84b744165a0597360caad96b06e69313-Abstract-Conference.html",
-            "code_link": "https://github.com/Xtra-Computing/FedSim",
-            "pdf_link": "https://arxiv.org/pdf/2106.06312.pdf"
-        },
-        {
-            "title": "Practical vertical federated learning with unsupervised representation learning",
-            "authors": "Zhaomin Wu, Qinbin Li, Bingsheng He",
-            "publication": "IEEE Transactions on Big Data",
-            "year": 2022,
-            "pub_abbr": "TBD 2022",
-            "pub_link": "https://ieeexplore.ieee.org/document/9789268",
-            "code_link": "https://github.com/JerryLife/FedOnce",
-            "pdf_link": "https://arxiv.org/pdf/2208.10278.pdf"
-        },
-        {
-            "title": "FedTree: A Federated Learning System For Trees",
-            "authors": "Qinbin Li, Zhaomin Wu, Yanzheng Cai, Yuxuan Han, Ching Man Yung, Tianyuan Fu, Bingsheng He",
-            "publication": "Sixth Conference on Machine Learning and Systems",
-            "year": 2023,
-            "pub_abbr": "MLSys 2023",
-            "pub_link": null,
-            "code_link": "https://github.com/Xtra-Computing/FedTree",
-            "pdf_link": null
-        },
-        {
-            "title": "The oarf benchmark suite: Characterization and implications for federated learning systems",
-            "authors": "Sixu Hu, Yuan Li, Xu Liu, Qinbin Li, Zhaomin Wu, Bingsheng He",
-            "publication": "ACM Transactions on Intelligent Systems and Technology",
-            "year": 2022,
-            "pub_abbr": "TIST 2022",
-
-            "pub_link": "https://dl.acm.org/doi/full/10.1145/3510540",
-            "code_link": "https://github.com/Xtra-Computing/OARF",
-            "pdf_link": "https://dl.acm.org/doi/pdf/10.1145/3510540"
-        },
-        {
-            "title": "Privacy-preserving gradient boosting decision trees",
-            "authors": "Qinbin Li, Zhaomin Wu, Zeyi Wen, Bingsheng He",
-            "publication": "Proceedings of the AAAI Conference on Artificial Intelligence",
-            "year": 2020,
-            "pub_abbr": "AAAI 2020",
-
-            "pub_link": "https://ojs.aaai.org/index.php/AAAI/article/view/5422",
-            "code_link": "https://github.com/QinbinLi/DPBoost",
-            "pdf_link": "https://ojs.aaai.org/index.php/AAAI/article/view/5422/5278"
-        },
-        {
-            "title": "A Survey on Federated Learning Systems: Vision, Hype and Reality for Data Privacy and Protection",
-            "authors": "Qinbin Li, Zeyi Wen, Zhaomin Wu, Sixu Hu, Naibo Wang, Yuan Li, Xu Liu, Bingsheng He",
-            "publication": "IEEE Transactions on Knowledge and Data Engineering",
-            "year": 2021,
-            "pub_abbr": "TKDE 2021",
-
-            "pub_link": "https://ieeexplore.ieee.org/abstract/document/9599369",
-            "code_link": null,
-            "pdf_link": "https://arxiv.org/pdf/1907.09693.pdf"
-        }
+            "pub_abbr": "IJERPH",
+            "pub_link": "https://www.mdpi.com/1660-4601/19/21/14411",
+            "doi": "doi:10.3390/ijerph192114411",
+        },  
     ],
     "award": [
         {
-            "title": "Meritorious Winner",
-            "organization": "Consortium for Mathematics and Its Applications (COMAP)",
-            "event": "Mathematical Contest in Modeling (MCM/ICM)",
-            "description": "Top 10% among all participants worldwide",
-            "year": 2018
+            "title": "2021 Excellent Bachelor's Thesis in Henan Province"
+            "organization": "Henan Province, China",
+            "event": "",
+            "description": "",
+            "year": 2021
         },
-        {
-            "title": "Outstanding Students",
-            "organization": "Huazhong University of Science and Technology (HUST)",
-            "event": null,
-            "description": "Top 1% among undergraduate students",
-            "year": 2016
-        },
-        {
-            "title": "National Scholarship",
-            "organization": "Ministry of Education of China",
-            "event": null,
-            "description": "Top 1% among undergraduate students",
-            "year": 2017
-        },
-        {
-            "title": "Third Prize",
-            "organization": " Ministry of Education of China and Chinese Institute of Electronics",
-            "event": "National College Students Connected Smarter System Innovation Competition",
-            "description": "Top 10% among all participants nationwide",
-            "year": 2016
-        },
-        {
-            "title": "Merit Student",
-            "organization": "Huazhong University of Science and Technology (HUST)",
-            "event": null,
-            "description": "Top 5% among undergraduate students",
-            "year": 2015
-        },
-        {
-            "title": "Outstanding undergraduate thesis",
-            "organization": "Department of Computer Science and technology, Huazhong University of Science and Technology (HUST)",
-            "event": null,
-            "year": 2019,
-            "description": "Top 5% among undergraduate students"
-        },
-        {
-            "title": "Distinguished Graduate",
-            "organization": "Huazhong University of Science and Technology (HUST)",
-            "event": null,
-            "description": "Top 40% among undergraduate students",
-            "year": 2019
-        }
     ],
     "skill": {
         "programming_language": {
             "familiar": [
                 "Python",
-                "C",
-                "C++",
-                "Java"
+                "Matlab",
             ],
             "used": [
-                "MATLAB",
+                "C++/Arduino",
                 "SQL",
-                "Verilog",
-                "x86 Assembly",
-                "JavaScript",
-                "Standard ML"
+                "C",
             ]
         },
         "tool": {
             "familiar": [
                 "PyTorch",
-                "Linux Command Line",
-                "Latex",
-                "Word",
-                "Excel",
-                "PowerPoint"
+                "MNE/Python",
+                "Arduino",
+                "EEG"
+                "fNIRS"
             ],
             "used": [
                 "TensorFlow",
-                "LightGBM",
-                "XGBoost",
-                "Qt",
-                "JavaFX",
-                "Win32 API",
-                "Apache Cassandra",
-                "MongoDB"
+                "MMLAB",
+                "SolidWorks"
+                "fMRI"
             ]
         },
         "language": [
             "Chinese (native)",
-            "English (competent)"
+            "English"
         ]
     },
     "service": {
-        "conference": [
-            "Program Committee Member, Pacific-Asia Conference on Knowledge Discovery and Data Mining (PAKDD 2021)",
-            "Program Committee Member, Pacific-Asia Conference on Knowledge Discovery and Data Mining (PAKDD 2022)"
+        "TA": [
+            "Regular TA for Computational Neuroscience at Neuromatch Academy, Aug.2022",
         ],
-        "journal": [
-            "Reviewer, IEEE Transactions on Parallel and Distributed Systems (TPDS)",
-            "Reviewer, ACM Transactions on Intelligent Systems and Technology (TIST)",
-            "Reviewer, IEEE Internet of Things Journal (IoT-J)"
+        "School": [
+            "GEMS Instructor for teaching middle school students, Jul. 2022",
         ],
-        "tutorial": [
-            "Tutorial Speaker, International Joint Conference on Artificial Intelligence (IJCAI 2021)"
+        "Community": [
+            "Speaker for WEekly Learn Open Science (WE LOST #8), Feb. 2021",
         ]
     }
 
